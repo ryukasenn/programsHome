a18f589b06780220d8c284987b996b2fef60b83a
ryuukasenn <446347372@qq.com>
2018-03-16 20:18:01 +0800

    补充commit

---
+++ src/com/cn/lingrui/sellPersonnel/pojos/region/Area_Xzqx_Info.java
@@ -0,0 +1,41 @@
+package com.cn.lingrui.sellPersonnel.pojos.region;
+
+public class Area_Xzqx_Info {
+
+	private String XZQX_PID;
+	private String XZQX_LEVEL;
+	private String XZQX_NAME;
+	private String XZQX_ID;
+	private String XZQX_TYPE;
+	public String getXZQX_NAME() {
+		return XZQX_NAME;
+	}
+	public void setXZQX_NAME(String xZQX_NAME) {
+		XZQX_NAME = xZQX_NAME;
+	}
+	public String getXZQX_ID() {
+		return XZQX_ID;
+	}
+	public void setXZQX_ID(String xZQX_ID) {
+		XZQX_ID = xZQX_ID;
+	}
+	public String getXZQX_TYPE() {
+		return XZQX_TYPE;
+	}
+	public void setXZQX_TYPE(String xZQX_TYPE) {
+		XZQX_TYPE = xZQX_TYPE;
+	}
+	public String getXZQX_LEVEL() {
+		return XZQX_LEVEL;
+	}
+	public void setXZQX_LEVEL(String xZQX_LEVEL) {
+		XZQX_LEVEL = xZQX_LEVEL;
+	}
+	public String getXZQX_PID() {
+		return XZQX_PID;
+	}
+	public void setXZQX_PID(String xZQX_PID) {
+		XZQX_PID = xZQX_PID;
+	}
+	
+}
